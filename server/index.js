ccb004a757c0ad90c5cde11c9c4059ac6c253150
HollyBang <mazafakand@gmail.com>
2019-06-25 17:26:09 +0300

    added sequelize config for migrations

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 const ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
-app.use('/', express.static(path.join(__dirname, 'view')));
+// app.use('/', express.static(path.join(__dirname, 'view')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/view/index.html'));
